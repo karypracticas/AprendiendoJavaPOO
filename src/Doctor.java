a1348e273b7212ef7b0b1c786a2403b4834d490d
Kary Rguez <practicasgithub@gmail.com>
2020-06-26 15:05:32 -0500

    Iniciar proyecto practica Java POO, crear primer clase, instanciar clase en método Main

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+//Modelo o Plantilla
+public class Doctor {
+    //Propiedades
+    int id;
+    String name;
+    String speciality;
+
+    //Método
+    public void showName() {
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Instanciar clase Doctor
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Juan Perez";
+        myDoctor.showName();
+    }
+}
