b7b2345ac5322ee170955365a56766911a641666
Kary Rguez <practicasgithub@gmail.com>
2020-06-29 17:49:34 -0500

    Creando elementos estaticos

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,24 @@
+import javax.print.Doc;
+
 //Modelo o Plantilla
 public class Doctor {
     //Propiedades
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
+
+    //Método Constructor
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
+        id++;
+    }
 
     //Método
     public void showName() {
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor:" + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import static ui.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,5 +7,17 @@ public class Main {
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Juan Perez";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        showMenu();
+
+        
+
     }
+
 }
