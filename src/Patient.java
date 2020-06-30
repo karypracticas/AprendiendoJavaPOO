7d9b21a33de6c54035007e5d207fda9811585cfc
Kary Rguez <practicasgithub@gmail.com>
2020-06-30 17:11:12 -0500

    Agregar clase Patient

---
+++ src/Patient.java
@@ -0,0 +1,17 @@
+public class Patient {
+    //Propiedades
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    Double weight;
+    Double height;
+    String blood;
+
+    //Constructor
+    Patient(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
